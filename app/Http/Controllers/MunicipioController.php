841770a7c8679ccf81caa674ce3bdd034cc3662c
felipeverse <felipealvesrrodrigues@outlook.com>
2022-05-20 11:37:48 -0400

    add comment and a simple  status code handle

--- app/Http/Controllers/MunicipioController.php
+++ app/Http/Controllers/MunicipioController.php
@@ -8,17 +8,26 @@ use App\Models\Municipio;
 
 class MunicipioController extends Controller
 {
+
+    /**
+     * Importa para o banco de dados os munícipios da uf passada via parâmetro.
+     *
+     * @param  string $uf
+     * @return string message
+     */
     public static function ImportMunicipiosFromIBGEAPI($uf)
     {
         try {
             $response = Http::get("https://servicodados.ibge.gov.br/api/v1/localidades/estados/{$uf}/municipios");
         
-            $municipios = json_decode($response->body());
+            if($response->status() != 200)
+                return "Error Processing Request";
 
+            $municipios = json_decode($response->body());
             $items = 0;
 
             foreach($municipios as $municipio) {
-                Municipio::updateOrCreate (
+                Municipio::firstOrCreate (
                     ['ibge_municipio_id' => $municipio->id], 
                     [
                         'ibge_municipio_nome' => $municipio->nome,
@@ -26,13 +35,11 @@ class MunicipioController extends Controller
                         'ibge_uf_id' => $municipio->microrregiao->mesorregiao->UF->id
                     ]
                 );
-
                 $items++;
             }
 
             return "{$items} items successfully imported.";
         } catch (\Exception $e) {
-            
             return $e->getMessage();
         }
     }
