0f43dc0ce50fe556c72387d1e7d40b1b12777867
felipeverse <felipealvesrrodrigues@outlook.com>
2022-05-21 19:54:16 -0400

    create authentication and protect api routes

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Lista todos os munícipios url/api/municipios
-Route::get('municipios', 'App\Http\Controllers\MunicipioController@index');
+// Rota da API para registrar novo usuário
+Route::post('/register', [App\Http\Controllers\API\AuthController::class, 'register']);
+
+// Rota da API para login de usuário
+Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
+
+// Rotas protegidas
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    
+    // Retorna dados do usuário logado
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+
+    // Rota da API para logout do usuário
+    Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout']);
+
+    // Lista todos os munícipios url/api/municipios
+    Route::get('municipios', 'App\Http\Controllers\MunicipioController@index');
+
+});
